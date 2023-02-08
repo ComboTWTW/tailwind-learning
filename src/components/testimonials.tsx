1754446de48d543d47c0f3e6f554977b19010981
TRMmember <gfakemail18@gmail.com>
2023-02-08 13:07:06 +0500

    fixed some issues

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,14 +1,14 @@
 import { TestCard } from "../constants/index";
 
 const Testimonials = () => (
-  <div className="max-w-6xl container mx-auto flex flex-col mt-32 items-center px-4 space-x-6">
+  <div className="container max-w-6xl  mx-auto flex flex-col mt-32 items-center px-4 ">
     <h2 className=" text-4xl font-bold text-center">
         What's different About Manage?
     </h2>
 
-    <div className="flex flex-col space-y-16 mt-10 md:flex-row md:space-x-6">
+    <div className="flex flex-col  space-y-16 mt-10 md:flex-row">
       {TestCard.map((card) => (
-        <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
+        <div className="flex flex-col mt-16 items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
           <img className="-mt-14 h-[64px] w-[64px]" src={card.avatar} alt="" />
           <h4 className="font-bold text-lg">{card.name}</h4>
           <p className="text-center text-darkGrayishBlue text-[14px] sm:text-sm">{card.text}</p>
